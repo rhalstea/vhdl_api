2ea9721b3795df24167e57bb8068f7238f03830d
Robert J. Halstead <rhalstea@gmail.com>
2012-03-27 16:43:22 -0700

    added basic test cases for connection list

---
+++ examples/test_connection_lists/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <connection.h>
+
+using namespace vhdl;
+
+int main() {
+
+  connection_list my_connections;
+
+  input_wire *clk = new input_wire("clk", 1);
+  input_wire *rst = new input_wire("rst", 1);
+
+  wire *tmp_0 = new wire("clk_tmp_0", 1);
+  wire *tmp_1 = new wire("clk_tmp_1", 1);
+  wire *tmp_2 = new wire("rst_tmp_0", 1);
+
+  my_connections.add_io_connection(clk, tmp_0);
+  my_connections.add_io_connection(clk, tmp_1);
+  my_connections.add_io_connection(rst, tmp_2);
+
+  std::cout << "signal for " << clk->name() << " is " 
+	    << my_connections.get_connection_name(clk) << std::endl;
+  std::cout << "signal for " << rst->name() << " is " 
+	    << my_connections.get_connection_name(rst) << std::endl;
+
+  std::cout << "signal for " << tmp_0->name() << " is " 
+	    << my_connections.get_connection_name(tmp_0) << std::endl;
+  std::cout << "signal for " << tmp_1->name() << " is " 
+	    << my_connections.get_connection_name(tmp_1) << std::endl;
+  std::cout << "signal for " << tmp_2->name() << " is " 
+	    << my_connections.get_connection_name(tmp_2) << std::endl;
+
+  return 0;
+}
