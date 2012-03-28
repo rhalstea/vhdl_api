fe64d96efc302967bdf7fcb092fc8de99ebfdc25
Robert J. Halstead <rhalstea@gmail.com>
2012-03-27 17:00:44 -0700

    added test cases for connection_list.add_connection()

--- examples/test_connection_lists/main.cpp
+++ examples/test_connection_lists/main.cpp
@@ -15,14 +15,23 @@ int main() {
   wire *tmp_1 = new wire("clk_tmp_1", 1);
   wire *tmp_2 = new wire("rst_tmp_0", 1);
 
+  wire *src_0 = new wire("src_wire", 32);
+  wire *dest_0 = new wire("dest_wire_0", 32);
+  wire *dest_1 = new wire("dest_wire_1", 32);
+
   my_connections.add_io_connection(clk, tmp_0);
   my_connections.add_io_connection(clk, tmp_1);
   my_connections.add_io_connection(rst, tmp_2);
+
+  my_connections.add_connection(src_0, dest_0);
+  my_connections.add_connection(src_0, dest_1);
 
   std::cout << "signal for " << clk->name() << " is " 
 	    << my_connections.get_connection_name(clk) << std::endl;
   std::cout << "signal for " << rst->name() << " is " 
 	    << my_connections.get_connection_name(rst) << std::endl;
+  std::cout << "signal for " << src_0->name() << " is " 
+	    << my_connections.get_connection_name(src_0) << std::endl;
 
   std::cout << "signal for " << tmp_0->name() << " is " 
 	    << my_connections.get_connection_name(tmp_0) << std::endl;
@@ -30,6 +39,11 @@ int main() {
 	    << my_connections.get_connection_name(tmp_1) << std::endl;
   std::cout << "signal for " << tmp_2->name() << " is " 
 	    << my_connections.get_connection_name(tmp_2) << std::endl;
+
+  std::cout << "signal for " << dest_0->name() << " is " 
+	    << my_connections.get_connection_name(dest_0) << std::endl;
+  std::cout << "signal for " << dest_1->name() << " is " 
+	    << my_connections.get_connection_name(dest_1) << std::endl;
 
   return 0;
 }
