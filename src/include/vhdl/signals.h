5558a0bff6de57abd2a14160496f8cefe10b68ca
Robert Halstead <rhalstea@gmail.com>
2013-05-23 00:04:33 -0700

    added class for input and output ports

--- src/include/vhdl/signals.h
+++ src/include/vhdl/signals.h
@@ -22,6 +22,21 @@ namespace vhdl {
          void set_name(std::string name);
          void set_size(unsigned size);
    };
+
+   class input_port : public signal {
+      public:
+         input_port(std::string name, unsigned bit_width);
+         ~input_port();
+         virtual void print(std::ostream &stream, std::string append = "");
+   };
+   
+   class output_port : public signal {
+      public:
+         output_port(std::string name, unsigned bit_width);
+         ~output_port();
+         virtual void print(std::ostream &stream, std::string append = "");
+
+   };
 }
 
 #endif
