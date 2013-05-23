e9aafda7e48561b4f25f065977eb94b9b9363e97
Robert Halstead <rhalstea@gmail.com>
2013-05-23 00:03:57 -0700

    exteneded signals test case to test input and output ports

--- examples/test_signals/main.cpp
+++ examples/test_signals/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <vector>
 #include "vhdl/signals.h"
 
 int main() {
-   vhdl::signal *s1 = new vhdl::signal("clk", 0);
-   vhdl::signal *s2 = new vhdl::signal("clk", 1);
-   vhdl::signal *s3 = new vhdl::signal("clk", 2);
 
-   s1->print(std::cout, ";\n");
-   s2->print(std::cout, ";\n");
-   s3->print(std::cout, ";\n");
+   std::vector<vhdl::signal*> my_signals;
+
+   my_signals.push_back( new vhdl::input_port("clk", 1) );
+   my_signals.push_back( new vhdl::input_port("rst", 1) );
+   my_signals.push_back( new vhdl::input_port("A", 8) );
+   my_signals.push_back( new vhdl::input_port("B", 8) );
+   my_signals.push_back( new vhdl::output_port("C", 8) );
+
+   my_signals.push_back( new vhdl::signal("tmp_0", 0) );
+   my_signals.push_back( new vhdl::signal("tmp_1", 1) );
+   my_signals.push_back( new vhdl::signal("tmp_2", 8) );
+
+   std::vector<vhdl::signal*>::iterator iter;
+   for (iter = my_signals.begin(); iter != my_signals.end(); ++iter) {
+      (*iter)->print(std::cout, " ;\n");
+   }
 
    return 0;
 }
