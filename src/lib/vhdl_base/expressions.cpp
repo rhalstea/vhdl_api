1673a8c2173ab75193ecf867f1471d5813886331
Robert Halstead <rhalstea@gmail.com>
2013-05-27 22:10:23 -0700

    added the first expression objects to the library

---
+++ src/lib/vhdl_base/expressions.cpp
@@ -0,0 +1,42 @@
+#include <expressions.h>
+
+using namespace vhdl;
+
+// ----- ----- ----- ----- ----- -----
+// ----- ----- ----- ----- ----- -----
+expression::expression() {
+   _signal = NULL;
+}
+
+expression::expression(signal *sig) :
+   _signal(sig) {}
+
+expression::~expression() {
+   // nothing to do yet
+}
+
+void expression::print(std::ostream &stream) {
+   stream << _signal->name();
+}
+
+unsigned expression::num_bits() {
+   return _signal->size();
+}
+
+// ----- ----- ----- ----- ----- -----
+// ----- ----- ----- ----- ----- -----
+
+add::add(expression *lhs, expression *rhs) :
+   _lhs(lhs), _rhs(rhs) {}
+
+add::~add() {
+   // nothing to do yet
+}
+
+void add::print(std::ostream &stream) {
+   stream << "(";
+   _lhs->print(stream);
+   stream << " + ";
+   _rhs->print(stream);
+   stream << ")";
+}
